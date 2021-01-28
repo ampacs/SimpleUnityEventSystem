9d9b4548fc9aff4713ab98d47a33b0da0fffc870
André Santiago <andrempacsantiago@outlook.com>
2021-01-28 10:08:31 +0100

    Added Simple Event System with example events

---
+++ Assets/Events/SimpleEventSystem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events
+{
+    public class SimpleEventSystem : MonoBehaviour
+    {
+        private static SimpleEventSystem _current;
+
+        private Dictionary<string, Action> _listeners;
+
+        public static void AddListener(string e, Action a)
+        {
+            if (ReferenceEquals(_current, null)) {
+                Debug.LogError("the current scene has no defined SimpleEventSystem singleton");
+                return;
+            }
+            if (_current._listeners.ContainsKey(e)) {
+                Debug.LogErrorFormat("attempted to add listener to undefined event \"{0}\"", e);
+                return;
+            }
+
+            _current._listeners[e] += a;
+        }
+
+        public static void RemoveListener(string e, Action a)
+        {
+            if (ReferenceEquals(_current, null)) {
+                Debug.LogError("the current scene has no defined SimpleEventSystem singleton");
+                return;
+            }
+            if (!_current._listeners.ContainsKey(e)) {
+                Debug.LogErrorFormat("attempted to remove listener from undefined event \"{0}\"", e);
+                return;
+            }
+
+            _current._listeners[e] -= a;
+        }
+
+        public static void TriggerEvent(string e) {
+            if (ReferenceEquals(_current, null)) {
+                Debug.LogError("the current scene has no defined SimpleEventSystem singleton");
+                return;
+            }
+            if (!_current._listeners.ContainsKey(e)) {
+                Debug.LogErrorFormat("attempted to trigger undefined event \"{0}\"", e);
+                return;
+            }
+            if (_current._listeners[e] == default) {
+                Debug.LogWarningFormat("attempted to trigger event \"{0}\" without listeners", e);
+                return;
+            }
+
+            _current._listeners[e].Invoke();
+        }
+
+        private void Awake()
+        {
+            if (_current == null) {
+                _current = this;
+            } else if (_current != this) {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+
+            // only events defined in Types
+            _listeners = new Dictionary<string, Action>();
+            foreach (string e in Types.Events) {
+                _listeners[e] = default;
+            }
+        }
+    }
+}
---
+++ Assets/Events/Types.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Events
+{
+    public struct Types
+    {
+        public const string ExampleEvent1 = "example-event-1";
+        public const string ExampleEvent2 = "example-event-2";
+
+        /**
+         * Contains all the events defined as public constant strings
+         */
+        internal static readonly string[] Events = GetEventNames();
+
+        /**
+         * Returns the all the events defined as public constant strings in the Types struct
+         */
+        private static string[] GetEventNames()
+        {
+            return (
+                from fi in typeof(Types).GetFields(BindingFlags.Public | BindingFlags.Static)
+                where fi.IsLiteral && !fi.IsInitOnly && Type.GetTypeCode(fi.GetValue(null).GetType()) == TypeCode.String
+                select fi.GetValue(null).ToString()
+            ).ToArray();
+        }
+    }
+}
